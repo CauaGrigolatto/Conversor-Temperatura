c0c9cf91e9f791fe506b25f9402d820d740e5bc0
Caua Grigolatto - AQ3022323 <caua.grigolatto@aluno.ifsp.edu.br>
2024-11-15 14:13:00 -0300

    Add internationalization

--- app/src/main/java/br/edu/ifsp/dmo1/conversortemperatura/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversortemperatura/view/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.dmo1.conversortemperatura.R
 import br.edu.ifsp.dmo1.conversortemperatura.databinding.ActivityMainBinding
@@ -30,78 +31,11 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.root.setBackgroundResource(R.drawable.background_gradient)
         loadComponents()
     }
 
-    override fun onClick(v: View?) {
-        if (v == convertButton) {
-            convertAndDisplay()
-        }
-    }
-
-    private fun convertAndDisplay() {
-        var fromScale: String = getFromScale()
-        var toScale: String = getToScale()
-        var value: Double = getInputtedTemperature()
-
-        this.converter = getConverter(fromScale)
-
-        var result = getConversion(value, toScale, converter)
-        var formatScale = getFormatScale(toScale)
-
-        displayResult(result, formatScale)
-    }
-
-    private fun getFormatScale(scale: String): String {
-        return when (scale) {
-            TemperatureConverter.CELSIUS -> CelsiusStrategy().getScale()
-            TemperatureConverter.FAHRENHEIT -> FahrenheitStrategy().getScale()
-            TemperatureConverter.KELVIN -> KelvinStrategy().getScale()
-            else -> null!!
-        }
-    }
-
-    private fun displayResult(result: Double, scale: String) {
-        resultText.text = "$result $scale"
-    }
-
-    private fun getConverter(fromScale: String): TemperatureConverter {
-        return when (fromScale) {
-            TemperatureConverter.CELSIUS -> CelsiusStrategy()
-            TemperatureConverter.FAHRENHEIT -> FahrenheitStrategy()
-            TemperatureConverter.KELVIN -> KelvinStrategy()
-            else -> null!!
-        }
-    }
-
-    private fun getConversion(value: Double, scale: String, converter: TemperatureConverter): Double {
-        return when (scale) {
-            TemperatureConverter.CELSIUS -> converter.convertToCelsius(value)
-            TemperatureConverter.FAHRENHEIT -> converter.convertToFahrenheit(value)
-            TemperatureConverter.KELVIN -> converter.convertToKelvin(value)
-            else -> null!!
-        }
-    }
-
-    private fun getInputtedTemperature(): Double {
-        return try {
-            temperatureInput.text.toString().toDouble()
-        }
-        catch (e: NumberFormatException) {
-            0.0
-        }
-    }
-
-    private fun getFromScale(): String {
-        return fromTemperatureSpinner.selectedItem.toString()
-    }
-
-    private fun getToScale(): String {
-        return toTemperatureSpinner.selectedItem.toString()
-    }
-
     private fun loadComponents() {
+        binding.root.setBackgroundResource(R.drawable.background_gradient)
         temperatureInput = binding.temperatureInput
         resultText = binding.resultTextView
         convertButton = binding.convertButton
@@ -125,5 +59,81 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         fromTemperatureSpinner.adapter = adapter
         toTemperatureSpinner.adapter = adapter
+    }
+
+    override fun onClick(v: View?) {
+        if (v == convertButton) {
+            convertAndDisplay()
+        }
+    }
+
+    private fun convertAndDisplay() {
+        var fromScale: String = getFromScale()
+        var toScale: String = getToScale()
+        var value: Double = getInputtedTemperature()
+
+        this.converter = getConverter(fromScale)
+
+        if (converter != null) {
+            var result = convert(value, toScale, converter)
+            var formatScale = getFormatScale(toScale)
+            displayResult(result, formatScale)
+        }
+        else {
+            Toast.makeText(
+                this,
+                R.string.messsage_conversion_error,
+                Toast.LENGTH_SHORT
+            )
+        }
+
+    }
+
+    private fun getFromScale(): String {
+        return fromTemperatureSpinner.selectedItem.toString()
+    }
+
+    private fun getToScale(): String {
+        return toTemperatureSpinner.selectedItem.toString()
+    }
+
+    private fun getInputtedTemperature(): Double {
+        return try {
+            temperatureInput.text.toString().toDouble()
+        }
+        catch (e: NumberFormatException) {
+            0.0
+        }
+    }
+
+    private fun getConverter(fromScale: String): TemperatureConverter {
+        return when (fromScale) {
+            TemperatureConverter.CELSIUS -> CelsiusStrategy()
+            TemperatureConverter.FAHRENHEIT -> FahrenheitStrategy()
+            TemperatureConverter.KELVIN -> KelvinStrategy()
+            else -> null!!
+        }
+    }
+
+    private fun convert(value: Double, scale: String, converter: TemperatureConverter): Double {
+        return when (scale) {
+            TemperatureConverter.CELSIUS -> converter.convertToCelsius(value)
+            TemperatureConverter.FAHRENHEIT -> converter.convertToFahrenheit(value)
+            TemperatureConverter.KELVIN -> converter.convertToKelvin(value)
+            else -> null!!
+        }
+    }
+
+    private fun getFormatScale(scale: String): String {
+        return when (scale) {
+            TemperatureConverter.CELSIUS -> CelsiusStrategy().getScale()
+            TemperatureConverter.FAHRENHEIT -> FahrenheitStrategy().getScale()
+            TemperatureConverter.KELVIN -> KelvinStrategy().getScale()
+            else -> null!!
+        }
+    }
+
+    private fun displayResult(result: Double, scale: String) {
+        resultText.text = "${String.format("%.2f", result)} $scale"
     }
 }
